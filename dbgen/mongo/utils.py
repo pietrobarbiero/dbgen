e3e3844d228aceb587f6695525d4ff2af5c85756
Pietro <pietro.barbiero9@live.it>
2020-03-11 15:31:15 +0100

    Bug fix: start mongodb without password

--- dbgen/mongo/utils.py
+++ dbgen/mongo/utils.py
@@ -5,18 +5,18 @@ def mongo_status(configs):
     if configs.password:
         command = 'sudo service mongod status'
         p = os.system('echo %s | sudo -S %s' % (configs.password, command))
-    return p
+        return p
 
 
 def mongo_start(configs):
     if configs.password:
         command = 'sudo service mongod start'
         p = os.system('echo %s | sudo -S %s' % (configs.password, command))
-    return p
+        return p
 
 
 def mongo_shutdown(configs):
     if configs.password:
         command = 'sudo service mongod stop'
         p = os.system('echo %s | sudo -S %s' % (configs.password, command))
-    return p
+        return p
